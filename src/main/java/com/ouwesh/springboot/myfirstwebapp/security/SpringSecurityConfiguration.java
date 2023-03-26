6fa0716493eae738057a245eed832183c62eeed9
Ouwesh Seeroo <ouweshseeroo@gmail.com>
2023-03-26 13:41:39 +0400

    spring security configured

---
+++ src/main/java/com/ouwesh/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.ouwesh.springboot.myfirstwebapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.function.Function;
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+    @Bean
+    public InMemoryUserDetailsManager createUserDetails() {
+        Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
+        var user = User.builder()
+                .passwordEncoder(passwordEncoder)
+                .username("ows")
+                .password("password")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
