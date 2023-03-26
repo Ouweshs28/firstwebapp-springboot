f007ab6accf9ab4c255133f778a5eca62ec86b60
Ouwesh Seeroo <ouweshseeroo@gmail.com>
2023-03-26 14:33:40 +0400

    added h2 console and modified spring security to allow h2

--- src/main/java/com/ouwesh/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/ouwesh/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -2,11 +2,14 @@ package com.ouwesh.springboot.myfirstwebapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -20,6 +23,21 @@ public class SpringSecurityConfiguration {
         return new InMemoryUserDetailsManager(user, user2);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated());
+        httpSecurity.formLogin(Customizer.withDefaults());
+        httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions().disable();
+        return httpSecurity.build();
+    }
+
     private UserDetails createNewUser(String username, String password) {
         Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
         return User.builder()
@@ -28,10 +46,5 @@ public class SpringSecurityConfiguration {
                 .password(password)
                 .roles("USER", "ADMIN")
                 .build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
